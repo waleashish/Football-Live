f98817853ecdad79dcc859dd7f9342464b81eb14
waleashish <ashishwale747@gmail.com>
2024-03-27 01:21:14 -0700

    Code refactoring

--- src/constants/constants.py
+++
@@ -1,9 +0,0 @@
-DOTENV_PATH="config/.env"
-CREDENTIALS_PATH="config/credentials.json"
-
-GCLOUD_PROJECT = "GCLOUD_PROJECT"
-
-FOOTBALL_API = "api.football-data.org"
-FOOTBALL_API_KEY = "FOOTBALL_API_KEY"
-FOOTBALL = "Football"
-
