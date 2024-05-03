a9533fe617ec74c5369b65ce6d4e2c9c574d0f5d
waleashish <ashishwale747@gmail.com>
2024-05-03 22:28:02 +0000

    Postgres installation docker scripts

---
+++ installation/sql_scripts/seed_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO leagues
+    (league_id, name, code, team_count) 
+VALUES
+    (2021, 'Premier League', 'PL', 20),
+        (2014, 'La Liga', 'PD', 20),
+        (2019, 'Serie A', 'SA', 20),
+        (2002, 'Bundesliga', 'BL1', 18),
+        (2015, 'Ligue 1', 'FL1', 18);
